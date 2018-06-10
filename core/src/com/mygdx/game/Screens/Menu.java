eb466482b232df5ca22feb6e858bc1761431e730
Leonardox <Leonmb123@gmail.com>
2018-06-10 16:59:04 -0300

    adicionando botoes

--- core/src/com/mygdx/game/Screens/Menu.java
+++ core/src/com/mygdx/game/Screens/Menu.java
@@ -1,13 +1,27 @@
 package com.mygdx.game.Screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class Menu implements Screen {
 
-    Menu(){
+    Game game;
+    Skin skin;
 
 
-        
+    Menu(Game game){
+        this.game = game;
+        skin = new Skin(Gdx.files.internal("glassy/skin/glassy-ui.json"));
+        Button jogar = new TextButton("Jogar",skin,"small");
+        jogar.setSize(100,50);
+        jogar.addListener(new InputListener());
+
+
     }
 
     @Override
