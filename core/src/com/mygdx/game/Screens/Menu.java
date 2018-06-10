fa321e3af01ecc8926e4e912878847e67a570fa0
Leonardox <Leonmb123@gmail.com>
2018-06-10 16:19:44 -0300

    implementa no menu e no play_screen os metodos screen

--- core/src/com/mygdx/game/Screens/Menu.java
+++ core/src/com/mygdx/game/Screens/Menu.java
@@ -1,4 +1,47 @@
 package com.mygdx.game.Screens;
 
-public class Menu {
+import com.badlogic.gdx.Screen;
+
+public class Menu implements Screen {
+
+    Menu(){
+
+
+        
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
