2aad07ba853d2b933d43488f6a8787e94fbbf59d
MatheusFranklinLH <matheusfranklinr@gmail.com>
2018-06-11 21:30:46 -0300

    Testando arquivos

---
+++ core/src/com/mygdx/game/Hud/Hud.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.Hud;
+
+public class Hud {
+}
---
+++ core/src/com/mygdx/game/Screens/Menu.java
@@ -0,0 +1,61 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+public class Menu implements Screen {
+
+    Game game;
+    Skin skin;
+
+
+    Menu(Game game){
+        this.game = game;
+        skin = new Skin(Gdx.files.internal("glassy/skin/glassy-ui.json"));
+        Button jogar = new TextButton("Jogar",skin,"small");
+        jogar.setSize(100,50);
+        jogar.addListener(new InputListener());
+
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
